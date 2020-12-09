c2cbf80ffc59a777a340b12a71eac6e1b973ba9a
Tanner Davis <tannerdavisr@gmail.com>
2020-12-08 18:10:59 -0700

    Dataset class for COCO dataset

--- src/datasets/coco_dataset.py
+++ src/datasets/coco_dataset.py
@@ -6,16 +6,14 @@ import json
 import numpy as np
 from PIL import Image
 import nltk
-import tqdm
-
-from vocab import Vocab
+from tqdm import tqdm
 
 class CoCoDataset(Dataset):
     '''
         A Pytorch dataset wrapper around the COCO Dataset
         Args: root:Path-like, the directory where you cloned the COCO api repo
     '''
-    def __init__(self, coco_root, train_or_test, img_transform, batch_size, vocab_from_file=True, vocab_file='./vocab.pkl', year='2014') -> None:
+    def __init__(self, coco_root, train_or_test, img_transform, batch_size, vocab, year='2014') -> None:
         assert train_or_test in ['train', 'test'], "Mode must be either 'train' or 'test'"
         self.mode = train_or_test
         self.batch_size = batch_size
@@ -23,7 +21,6 @@ class CoCoDataset(Dataset):
         self.root = coco_root
         self.year = year
         if self.mode == 'train':
-            if vocab_from_file == True: assert os.path.exists(vocab_file), "Vocab file does not exist"
             self.img_folder = os.path.join(self.root, 'images', f'train{self.year}')
             self.captions_file = os.path.join(self.root, 'annotations', f'captions_train{self.year}.json')
             self.coco = COCO(self.captions_file)
@@ -33,16 +30,14 @@ class CoCoDataset(Dataset):
                     for idx in tqdm(np.arange(len(self.ids)))]
             self.caption_lengths = [len(token) for token in all_toks]
         else:
-            assert os.path.exists(vocab_file), "Vocab file does not exist, make sure to generate one from training"
             print("Overriding batch_size to '1' for test mode")
             self.batch_size = 1 # Override the batch size
-            self.vocab_from_file = True # Vocab must be from a file for test mode
             self.img_folder = os.path.join(self.root, 'images', f'test{self.year}')
             self.captions_file = os.path.join(self.root, 'annotations', f'image_info_test{self.year}.json')
             test_info = json.loads(open(self.captions_file).read())
             self.paths = [item['file_name'] for item in test_info['images']]
         
-        self.vocab = Vocab(vocab_file)
+        self.vocab = vocab
 
 
     def __getitem__(self, index):
@@ -59,11 +54,11 @@ class CoCoDataset(Dataset):
             img = self.transform(img)
 
             # Convert caption to tensor according to our vocab
-            tokens = nltk.tokenize_word(str(caption).lower())
+            tokens = nltk.word_tokenize(str(caption).lower())
             caption = []
-            caption.append(self.vocab(self.vocab.start_word))
-            caption.extend([self.vocab(token) for token in tokens])
-            caption.append(self.vocab(self.vocab.end_word))
+            caption.append(self.vocab[self.vocab.start_word])
+            caption.extend([self.vocab[token] for token in tokens])
+            caption.append(self.vocab[self.vocab.end_word])
 
             return img, torch.Tensor(caption).long()
         else: 
@@ -81,9 +76,9 @@ class CoCoDataset(Dataset):
 
     def get_train_indices(self):
         '''
-            Randomly samples a batch_size worth of captions all of length cap_length
+        Randomly samples a batch_size worth of captions *all of length length*
         '''
-        assert self.mode == True, "Must be in training mode to use this function"
+        assert self.mode == 'train', "Must be in training mode to use this function"
         cap_length = np.random.choice(self.caption_lengths)
         all_idxs = np.where([self.caption_lengths[i] == cap_length 
                     for i in np.arange(len(self.caption_lengths))])[0]
